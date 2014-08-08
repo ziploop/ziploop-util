0fbb182a77afe149813ea06e189298956c470dc9
Jake McGuire <mcguireghs@gmail.com>
2014-08-08 11:42:48 -0700

    Add existing functions

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 module.exports = {
-	test: function(){
-	  console.log("Testing");
+	throttleForEach: function(array, wait, iterator){
+    (function getNextItem(index){
+      iterator(array[index], index);
+      var next = index + 1;
+      if (next < array.length) {
+        setTimeout(function(){
+          getNextItem(next);
+        }, wait);
+      }
+    })(0);
+  },
+  validParams: function(obj, keys) {
+	  var missing = [];
+	  keys.forEach(function(key){
+	    if (!obj.hasOwnProperty(key)) missing.push(key);
+	  });
+	  if (missing.length > 0) {
+	    console.log("MISSING ARGUMENTS:\n", missing);
+	    return false;
+	  } else {
+	    return true;
+	  }
 	}
 };
